a4128187b3f70c81a6214fb06e11090de4d0201c
harrison0502 <harrison0502@gmail.com>
2023-06-06 15:53:26 +0800

    feat: add detail page

--- app.js
+++ app.js
@@ -34,12 +34,21 @@ app.get('/', (req, res) => {
   Todo.find() // 取出 Todo model 裡的所有資料
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
     .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
+    .catch(error => console.log(error))
 })
 
 app.get('/todos/new',(req,res)=>{
   return res.render('new')
 })
 
+
+app.get('/todos/:id',(req,res)=>{
+  const id=req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then(todo => res.render('detail',{ todo }))
+    .catch(console.error(error))
+})
 
 app.post('/todos',(req,res)=>{
   const name=req.body.name
