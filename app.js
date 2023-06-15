c7c77213bab266a8230031d5e8936f153011b8ca
harrison0502 <harrison0502@gmail.com>
2023-06-15 17:15:55 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,35 +1,17 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
-const mongoose = require('mongoose')// 載入 mongoose
 const exphbs = require('express-handlebars');
 const Todo = require('./models/todo') // 載入 Todo model
 const bodyParser = require('body-parser')
 // 引用路由器
 const routes = require('./routes')
+require('./config/mongoose')
 // 載入 method-override
 const methodOverride = require('method-override')
-// 加入這段 code, 僅在非正式環境時, 使用 dotenv
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
 const app = express()
 
 
-// 設定連線到 mongoDB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
-
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 
 // 設定每一筆請求都會透過 methodOverride 進行前置處理
