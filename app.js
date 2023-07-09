4c936a216b333b6ba6fe81c7225ca0dce37dc7a7
harrison0502 <harrison0502@gmail.com>
2023-07-10 02:59:02 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
-const exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars')
+const session = require('express-session')
 const Todo = require('./models/todo') // 載入 Todo model
 const bodyParser = require('body-parser')
 // 引用路由器
 const routes = require('./routes')
 require('./config/mongoose')
+const usePassport = require('./config/passport')
 // 載入 method-override
 const methodOverride = require('method-override')
 const app = express()
@@ -18,8 +20,16 @@ const PORT = process.env.PORT || 3000
 app.use(methodOverride('_method'))
 // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({
+  secret: 'my-secret-key',
+  resave: false,
+  saveUninitialized: true,
+}))
+// 呼叫 Passport 函式並傳入 app
+usePassport(app)
 // 將 request 導入路由器
 app.use(routes)
+
 
 
 
