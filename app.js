ad435d7cceb8972e53b5acff13b1655807dc3b62
harrison0502 <harrison0502@gmail.com>
2023-07-11 13:50:29 +0800

    feat: add facebook login & dotenv

--- app.js
+++ app.js
@@ -2,15 +2,19 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const session = require('express-session')
-const Todo = require('./models/todo') // 載入 Todo model
+const Todo = require('./models/todo') 
 const bodyParser = require('body-parser')
-// 引用路由器
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const routes = require('./routes')
 require('./config/mongoose')
 const usePassport = require('./config/passport')
 const flash = require('connect-flash')
-// 載入 method-override
 const methodOverride = require('method-override')
+
 const app = express()
 const PORT = process.env.PORT || 3000
 
@@ -22,7 +26,7 @@ app.use(methodOverride('_method'))
 // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(session({
-  secret: 'my-secret-key',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true,
 }))
@@ -48,4 +52,5 @@ app.set('view engine', 'hbs')
 // 設定 port 3000
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
+  
 })
--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,13 @@ const router = express.Router()
 const home = require('./modules/home')
 const todos = require('./modules/todos')
 const users = require('./modules/users')
+const auth = require('./modules/auth')   
 const { authenticator } = require('../middleware/auth')  // 掛載 middleware
+
 
 router.use('/users', users)
 router.use('/todos', authenticator, todos) 
+router.use('/auth', auth)
 router.use('/', authenticator, home) 
 // 匯出路由器
 module.exports = router
